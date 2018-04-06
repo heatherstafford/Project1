88f03c3e705ce0adb268937a9569d3b6d9a21fab
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-04-06 08:15:02 -0400

    Updated from Brython Server: 4/6/2018 8:15:01 AM

--- bullsAndCows.py
+++ bullsAndCows.py
@@ -6,8 +6,6 @@ from ggame import *
 from random import randint
 
 RADIUS = 20
-ROWS = 5
-COLS = 5
 
 def pickCode():
     num = randint(0,4)
@@ -23,30 +21,37 @@ def pickCode():
         col = Color(0x003bff,1) #Blue
 
 def green(Event):
-    Sprite(CircleAsset(RADIUS, LineStyle(2,Color(0x000000,1)), Color(0x006600,1)))
+    greenCircle = (CircleAsset(RADIUS, LineStyle(2,Color(0x000000,1)), Color(0x006600,1)))
+    Sprite(greenCircle,(10,10))
     
 def red(Event):
-    Sprite(CircleAsset(RADIUS,LineStyle(2,Color(0x000000,1)), Color(0xFF0000,1)))
-
+    redCircle = (CircleAsset(RADIUS,LineStyle(2,Color(0x000000,1)), Color(0xFF0000,1)))
+    Sprite(redCircle,(10,10))
+    
 def yellow(Event):
-    Sprite(CircleAsset(RADIUS,LineStyle(2,Color(0x000000,1)),Color(0xFFFF00,1)))
+    yellowCircle = (CircleAsset(RADIUS,LineStyle(2,Color(0x000000,1)),Color(0xFFFF00,1)))
+    Sprite(yellowCircle,(10,10))
     
 def purple(Event):
-    Sprite(CircleAsset(RADIUS,LineStyle(2,Color(0x000000,1)), Color(0xc300ff,1)))
+    purpleCircle = (CircleAsset(RADIUS,LineStyle(2,Color(0x000000,1)), Color(0xc300ff,1)))
+    Sprite(purpleCircle,(10,10))
     
 def blue(Event):
-    Sprite(CircleAsset(RADIUS,LineStyle(2,Color(0x000000,1)), Color(0x003bff,1)))
- 
+    blueCircle = (CircleAsset(RADIUS,LineStyle(2,Color(0x000000,1)), Color(0x003bff,1)))
+    Sprite(blueCircle,(10,10))
+    
+    """
 def newColor():
-    data['colors'] += ROWS
-
+    data['moves'] += 25
+    """
 #sets up and runs the game
 if __name__ == '__main__':  
-    
+
+    """
     #hold variables in a dictionary
     data = {}
-    data['colors']
-
+    data['moves']
+    """
     circle = CircleAsset(RADIUS, LineStyle(2,Color(0x000000,1)),Color(0xFFFFFF,1))
     
     for i in range(4):
