312255bd8f91efa78571e7b343a4dc9f2320b2b2
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-04-11 11:51:13 -0400

    Updated from Brython Server: 4/11/2018 11:51:12 AM

--- bullsAndCows.py
+++ bullsAndCows.py
@@ -11,56 +11,101 @@ COLS = 10
 
 #Computer randomly generates the colors for guessing
 def pickCode():
-    num = (randint(0,4), randint(0,4), randint(0,4), randint(0,4))
-    if num == 0: 
-        col = Color(0x006600,1) #Green
-    elif num == 1:
-        col = Color(0xFF0000,1) #Red
-    elif num == 2:
-        col = Color(0xFFFF00,1) #yellow
-    elif num == 3:
-        col = Color(0xc300ff,1) #Purple
+    num1 = randint(0,4)
+    if num1 == 0: 
+        data['code1'] = 'Green' #Green
+    elif num1 == 1:
+        data['code1'] = 'Red' #Red
+    elif num1 == 2:
+        data['code1'] = 'Yellow' #yellow
+    elif num1 == 3:
+        data['code1'] = 'Purple' #Purple
     else:
-        col = Color(0x003bff,1) #Blue
+        data['code1'] = 'Blue' #Blue
+    
+    num2 = randint(0,4)
+    if num2 == 0: 
+        data['code2'] = 'Green' #Green
+    elif num2 == 1:
+        data['code2'] = 'Red' #Red
+    elif num2 == 2:
+        data['code2'] = 'Yellow' #yellow
+    elif num2 == 3:
+        data['code2'] = 'Purple' #Purple
+    else:
+        data['code2'] = 'Blue' #Blue
+        
+    num3 = randint(0,4)
+    if num3 == 0: 
+        data['code3'] = 'Green' #Green
+    elif num3 == 1:
+        data['code3'] = 'Red' #Red
+    elif num3 == 2:
+        data['code3'] = 'Yellow' #yellow
+    elif num3 == 3:
+        data['code3'] = 'Purple' #Purple
+    else:
+        data['code3'] = 'Blue' #Blue
+        
+    num4 = randint(0,4)
+    if num4 == 0: 
+        data['code4'] = 'Green' #Green
+    elif num4 == 1:
+        data['code4'] = 'Red' #Red
+    elif num4 == 2:
+        data['code4'] = 'Yellow' #yellow
+    elif num4 == 3:
+        data['code4'] = 'Purple' #Purple
+    else:
+        data['code4'] = 'Blue' #Blue
     
 #circle changes to green
 def green(Event):
     greenCircle = (CircleAsset(RADIUS, LineStyle(2,Color(0x000000,1)), Color(0x006600,1)))
     Sprite(greenCircle,(data['rows'], data['cols']))
-    data['rows'] += 50
+    while data['rows'] < 120:
+        data['rows'] += 50
+        break
     
 #circle changes to red
 def red(Event):
     redCircle = (CircleAsset(RADIUS,LineStyle(2,Color(0x000000,1)), Color(0xFF0000,1)))
     Sprite(redCircle,(data['rows'], data['cols']))
-    While data['rows'] < 120:
+    while data['rows'] < 120:
         data['rows'] += 50
+        break
     
 #circle changes to yellow
 def yellow(Event):
     yellowCircle = (CircleAsset(RADIUS,LineStyle(2,Color(0x000000,1)),Color(0xFFFF00,1)))
     Sprite(yellowCircle,(data['rows'], data['cols']))
-    data['rows'] += 50
+    while data['rows'] < 120:
+        data['rows'] += 50
+        break
     
 #circle changes to purple
 def purple(Event):
     purpleCircle = (CircleAsset(RADIUS,LineStyle(2,Color(0x000000,1)), Color(0xc300ff,1)))
     Sprite(purpleCircle,(data['rows'], data['cols']))
-    data['rows'] += 50
+    while data['rows'] < 120:
+        data['rows'] += 50
+        break
  
 #circle changes to blue   
 def blue(Event):
     blueCircle = (CircleAsset(RADIUS,LineStyle(2,Color(0x000000,1)), Color(0x003bff,1)))
     Sprite(blueCircle,(data['rows'], data['cols']))
-    data['rows'] += 50
-"""
-def enter(Event):
-    
+    while data['rows'] < 120:
+        data['rows'] += 50
+        break
 
+def enter(Event):
+    checkCode()
+    
 #checks to see if the computers code is the same as the one entered
 def checkCode():
-    if 
-"""
+    
+
 #sets up and runs the game
 if __name__ == '__main__':  
 
@@ -68,6 +113,12 @@ if __name__ == '__main__':
     data = {}
     data['rows'] = 10
     data['cols'] = 10
+    data['code1'] = ''
+    data['code2'] = ''
+    data['code3'] = ''
+    data['code4'] = ''
+    data['cows'] = 0
+    data['bulls'] = 0
 
     circle = CircleAsset(RADIUS, LineStyle(2,Color(0x000000,1)),Color(0xFFFFFF,1))
     
@@ -81,8 +132,7 @@ if __name__ == '__main__':
     App().listenKeyEvent('keydown','y', yellow)
     App().listenKeyEvent('keydown','p', purple)
     App().listenKeyEvent('keydown','b', blue)
-    """
     App().listenKeyEvent('keydown','enter', enter)
-    """
+
     App().run()
     
